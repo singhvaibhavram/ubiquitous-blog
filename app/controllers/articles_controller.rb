15f7c4c85d09d938c732481a4d5d653363176f74
singhvaibhavram <singh.vaibhav.ram@gmail.com>
2020-10-03 10:29:51 +0530

    Edit and Update the Articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,8 +14,22 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
   def show
-    @article = Article.find(params[:id])    
+    @article = Article.find(params[:id])
   end
 
   private
