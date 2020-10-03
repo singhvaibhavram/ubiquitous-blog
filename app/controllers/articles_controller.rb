6d5c59c238f50ad7a87a171be119c86f81703d78
singhvaibhavram <singh.vaibhav.ram@gmail.com>
2020-10-03 11:20:28 +0530

    Article List, Edit Button and Show Button

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def index
+    @articles = Article.all
+  end
+
   def create
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
