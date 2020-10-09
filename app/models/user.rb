38ffe205e1ae2119c0361e6c383f949883b7dd2b
singhvaibhavram <singh.vaibhav.ram@gmail.com>
2020-10-09 09:22:10 +0530

    Secure Password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,22 @@
 class User < ActiveRecord::Base
-  # One to Many associations
-  has_many :articles
+    # One to Many associations
+    has_many :articles
 
-  # Email downcase
-  before_save { self.email = email.downcase }
+    # Email downcase
+    before_save { self.email = email.downcase }
 
-  # Username Validation for User Creation
-  validates :username, presence: true,
-             uniqueness: {case_sensitive: false},
-             length: {minimum: 3, maximum: 25}
+    # Username Validation for User Creation
+    validates :username, presence: true,
+               uniqueness: {case_sensitive: false},
+               length: {minimum: 3, maximum: 25}
 
- # Email validation for Email Creation
- VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
- validates :email, presence: true,
-            length: {maximum: 105},
-            uniqueness: {case_sensitive: false},
-            format: {with: VALID_EMAIL_REGEX}
+   # Email validation for Email Creation
+   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+   validates :email, presence: true,
+              length: {maximum: 105},
+              uniqueness: {case_sensitive: false},
+              format: {with: VALID_EMAIL_REGEX}
+
+   # Password Encryption and Authentication
+   has_secure_password
 end
