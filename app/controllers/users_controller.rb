addf32b0bd2851a59cd8ce32bafb40c61ff74373
singhvaibhavram <singh.vaibhav.ram@gmail.com>
2020-10-15 11:10:51 +0530

    Edit Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,22 @@ class UsersController < ApplicationController
     end
   end
 
+  # Edit Users
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  # Update Edited Users
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Hey #{@user.username}, your account was updated successfully!"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
